55bf71f81022e6511913fd499338726350d16207
Pimlada Kirativithayanan <dx6823765471@citu.tu.ac.thclear>
2026-08-27 17:55:48 +0700

    Part D

---
+++ d_function.js
@@ -0,0 +1,32 @@
+function getGrade(score) {
+
+if(score >= 85){
+    return "A";
+}
+else if(score >= 80){
+    return  "B+";
+}
+else if(score >= 75){
+    return "B";
+}
+else if(score >= 70){
+    return "C+";
+}
+else if(score >= 65){
+    return "C";
+}
+else if(score >= 60){
+    return "D+";
+}
+else if(score >= 55){
+    return "D";
+}
+else{
+    return "F";
+}
+}
+
+const scores = [72, 88, 95, 60, 81];
+for (let i = 0; i < scores.length; i++) {
+    console.log(`คะแนน ${scores[i]} = เกรด ${getGrade(scores[i])}`);
+}
