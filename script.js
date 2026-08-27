6da2e43b3189d6226b8302ab6639733a83a7bc1a
Pimlada Kirativithayanan <dx6823765471@citu.tu.ac.thclear>
2026-08-27 18:13:37 +0700

    complete Part E

---
+++ script.js
@@ -0,0 +1,34 @@
+function getGrade(score) {
+
+if(score >= 85){
+    return "A";
+}
+else if(score >= 80){
+    return  "B+";
+}
+else if(score >= 75){
+    return "B";
+}
+else if(score >= 70){
+    return "C+";
+}
+else if(score >= 65){
+    return "C";
+}
+else if(score >= 60){
+    return "D+";
+}
+else if(score >= 55){
+    return "D";
+}
+else{
+    return "F";
+}
+}
+
+const scores = [72, 88, 95, 60, 81];
+let teat = "";
+for (let i = 0; i < scores.length; i++) {
+    teat += `คะแนน ${scores[i]} = เกรด ${getGrade(scores[i])}\n`;
+}
+document.getElementById("output").textContent = teat;
